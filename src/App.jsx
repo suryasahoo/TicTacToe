1ad355cc3edd010040c1adcc0784d48ed94e9a8d
Surya <64300077+suryasahoo@users.noreply.github.com>
2023-12-09 21:18:39 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Quotes, Game } from './components/'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id='game'>
+        <Game/>
+      </div>
+      <div id="quote">
+        <Quotes />
+      </div>
+    </>
+  )
+}
+
+export default App
